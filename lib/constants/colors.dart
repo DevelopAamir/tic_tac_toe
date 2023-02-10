516a385e0ffc232940f7564bafee20660100359f
MD Aamir <88143370+DevelopAamir@users.noreply.github.com>
2023-02-10 11:36:01 +0545

    Add files via upload

---
+++ lib/constants/colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class ColorPallete{
+  static Color backgroundColor = const Color(0xff262627);
+  static Color color = const Color.fromRGBO(255, 255, 255, 0.39);
+}
